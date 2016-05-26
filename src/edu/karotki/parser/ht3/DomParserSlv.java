5b294a957c72ad07632ba071659312e7bbf39b4a
Viachaslau Karotki <Karotkiv@gmail.com>
2016-05-26 17:06:30 +0300

    commit

--- src/edu/karotki/parser/ht3/DomParserSlv.java
+++ src/edu/karotki/parser/ht3/DomParserSlv.java
@@ -1,11 +1,14 @@
 package edu.karotki.parser.ht3;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.util.Scanner;
+import java.io.FileReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DomParserSlv {
+	public static final String BEGINTAG="(\\<(/?[^\\>]+)\\>)";
+	public static final String ENDTAG="(\\<[^/](/?[^\\>]+)\\>)";
 	
 	public Node parse(File file){
 		String str=readFile(file);				
@@ -15,13 +18,15 @@ public class DomParserSlv {
 	private String readFile(File file){
 		String s="";
 		try{
-			Scanner sc= new Scanner(file);
-			while(sc.hasNext()){
-				s=s+sc.nextLine().trim();			
+			BufferedReader in=new BufferedReader(new FileReader(file));
+			String k;		
+			while((k=in.readLine())!=null){
+				s=s+k.trim();			
 			}
-			sc.close();			
+			in.close();		
 		}
 		catch(Exception e){e.printStackTrace();}
+		System.out.println(s);
 		return s;
 	}
 	
@@ -29,46 +34,39 @@ public class DomParserSlv {
 		Node node = new Node();
 		node.setTextContent(text);
 		int i=0; 
-		Pattern p=null; String tag=new String("");
-		Matcher m=null; 
+	//	 String tag=new String("");
+		Pattern p = Pattern.compile(BEGINTAG);
+		Matcher m = p.matcher(text);
+		if ((m.find())) {
+			System.out.println(analizeTag(m.group()));
+		}
 		do{
 			
-	   if (i==0) {p = Pattern.compile("<(\"[^\"]*\"|'[^']*'|[^'\">])*>");
-	   } else {
-		   p=Pattern.compile("<"+tag+"(\"[^\"]*\"|'[^']*'|[^'\">])*>");
-	   }
-		
-		    m = p.matcher(text);
-		
-		if(m.find()){
-		
-			node.appendChild(new Node());
-			String str=m.group(); int beginIndex=text.indexOf(str);
-			node.getChildNodes().get(i).setTag(analizeTag(str));//   извлекаем имя тега и присваиваем 
+			if (m.find()) {
+				node.appendChild(new Node());
+				node.getChildNodes().get(i).setTag(analizeTag(m.group()));
+				int end=text.indexOf("</"+analizeTag(m.group())+">");
+				int start=text.indexOf(m.group())+m.group().length();
+				node.getChildNodes().get(i).setTextContent(text.substring(start, end));
+			}
+			if (node.getChildNodes().get(i).isNode()){ }
+		    i++;
+				
 			
-			tag=node.getChildNodes().get(i).getTag(); //присваиваем имя тега промежуточной переменной
-			String textContent=text.substring(beginIndex);// обрезка текста
-			int endIndex=textContent.indexOf("<"+tag+"/>");
-			textContent=textContent.substring(0+str.length(), endIndex-1);
-			node.getChildNodes().get(i).setTextContent(textContent);
-			text=text.substring(endIndex+tag.length()+2);	
-			if (node.getChildNodes().get(i).isNode()) {node.getChildNodes().get(i);}
-		}
-		
-		i++;
 		}while(!text.isEmpty());		
 		return node;
 	}
 	
 	private String analizeTag(String text){
-		int endIndex=0;
-		for(int i=1;i<text.length();i++){
+		int endIndex=text.length();
+		for(int i=0;i<text.length();i++){
 			if ((text.charAt(i)==' ')||(text.charAt(i)=='>')){
-				endIndex=i-1;
+				endIndex=i-1; break;
 			}
+			
 		}
-		text.substring(1, endIndex);
-		return text;
+		return text.substring(1, endIndex+1);
+		
 	}
 	
 }
